9fe994b9bb49fb8e76aaa36d5c95e50a1aa329e3
Tom <thomashowe456@gmail.com>
2023-08-13 18:33:08 +0100

    Update Q2.sql

--- SQL_questions/Q2.sql
+++ SQL_questions/Q2.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE TABLE events AS
 SELECT
     event_id,
-    MIN(event_at) AS first_event_at,
+    MIN(event_at) AS event_at,
     event_name,
     user_id
 FROM
     events
 GROUP BY
-    event_id, event_name, event_at, user_id;
+    event_id, event_name, user_id;
